72e1c89a30a96f697c0b59d8b4c512db8940ed5e
Yogendra Pratap Singh <42297673+PratapSingh13@users.noreply.github.com>
2019-06-13 01:34:36 +0530

    Add files via upload

---
+++ Pass_Fail.cpp
@@ -0,0 +1,29 @@
+#include<iostream.h>
+#include<conio.h>
+int main()
+{
+	int marks[5],i;
+	float sum=0,avg;
+	cout<<"Please Enter Marks obtained in 5 subjects:"
+	for(i=0; i<5; i++)
+	{
+cin>>mark[i];
+Sum=sum+mark[i];
+	}
+avg=Sum/5;
+cout<<"Your Grade is";
+if(avg>80)
+{
+  cout<<"You got A grade";
+}
+else if(avg>60 && avg<=80)
+{
+  cout<<You got C grade";
+elseif(avg>40 && avg<=60)
+{
+  cout<<You got C grade";
+else
+{
+  cout<<You got D grade";
+}
+getch();  
